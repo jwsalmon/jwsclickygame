06ff1bda05cc8c10c20b1adf127b3b684a35622f
Jesse W. Salmon <j_aw_s@icloud.com>
2018-12-19 22:32:52 -0700

    Updates

---
+++ precache-manifest.8e0d5b4f8e7c1c8f587c4cb24787bf6e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee87c208732adfad95b330377663686b",
+    "url": "/jwsclickygame/static/media/Charger.ee87c208.jpg"
+  },
+  {
+    "revision": "ad6c6f38e6df8eeec7c6",
+    "url": "/jwsclickygame/static/css/main.43e4e740.chunk.css"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/jwsclickygame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "3362fbc603fd9d8adb01",
+    "url": "/jwsclickygame/static/js/runtime~main.3362fbc6.js"
+  },
+  {
+    "revision": "c288624810675eb17b6890b0034f718f",
+    "url": "/jwsclickygame/static/media/442.c2886248.jpg"
+  },
+  {
+    "revision": "23b4613b80ab4f65b3c7a378c6d871e3",
+    "url": "/jwsclickygame/static/media/67Corvette.23b4613b.jpg"
+  },
+  {
+    "revision": "3a44b7ea688d57758c6ec4eb89a9d25a",
+    "url": "/jwsclickygame/static/media/70GTX.3a44b7ea.jpg"
+  },
+  {
+    "revision": "bd08097cb2f45da387bb5c1552f2bb8e",
+    "url": "/jwsclickygame/static/media/AmericanMuscleCar.bd08097c.jpg"
+  },
+  {
+    "revision": "f8954aaad5450a070ba0771e9742c669",
+    "url": "/jwsclickygame/static/media/CarClickyGame.f8954aaa.mov"
+  },
+  {
+    "revision": "74250345d13c22e74f4e1726b9f56eca",
+    "url": "/jwsclickygame/static/media/CarClickyGame.74250345.png"
+  },
+  {
+    "revision": "ad6c6f38e6df8eeec7c6",
+    "url": "/jwsclickygame/static/js/main.ad6c6f38.chunk.js"
+  },
+  {
+    "revision": "cdbb1cd2e615243f3c40315a0b335c03",
+    "url": "/jwsclickygame/static/media/Firebird.cdbb1cd2.jpg"
+  },
+  {
+    "revision": "74fccbb45e6f230c80569b13530472e1",
+    "url": "/jwsclickygame/static/media/MGA.74fccbb4.jpg"
+  },
+  {
+    "revision": "14248d936765fd6827a679457b197de5",
+    "url": "/jwsclickygame/static/media/MG_MGA.14248d93.jpg"
+  },
+  {
+    "revision": "0b3d3938c1999ec0ee4532c032dd3c70",
+    "url": "/jwsclickygame/static/media/SS454.0b3d3938.jpg"
+  },
+  {
+    "revision": "4bd2340c19253985bde2857ee198b70a",
+    "url": "/jwsclickygame/static/media/TorinoCobra.4bd2340c.jpg"
+  },
+  {
+    "revision": "088eb3c1d2227ba123f8b94b68bff7ea",
+    "url": "/jwsclickygame/static/media/boss429.088eb3c1.jpg"
+  },
+  {
+    "revision": "99b95c96ba4910dbebbb6e6f81fdc30e",
+    "url": "/jwsclickygame/static/media/dart.99b95c96.jpg"
+  },
+  {
+    "revision": "04a84605b5442e564f68627e9b327e53",
+    "url": "/jwsclickygame/static/media/yenko.04a84605.jpg"
+  },
+  {
+    "revision": "e26687bf5b2c86814608032d956135ec",
+    "url": "/jwsclickygame/index.html"
+  }
+];
